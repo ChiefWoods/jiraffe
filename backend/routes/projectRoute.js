f70127cf629ff011d590fb26e64b99e698b02e8c
CWeyK <108680809+CWeyK@users.noreply.github.com>
2024-06-03 16:27:36 +0800

    project route

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -7,7 +7,7 @@ import { Task } from '../models/taskModel.js';
 const router = express.Router();
 
 //Route for get user's projects
-router.get('/:user_id', async (request, response) => {
+router.get('/user/:user_id', async (request, response) => {
     try{
         const { user_id } = request.params; 
 
@@ -25,6 +25,20 @@ router.get('/:user_id', async (request, response) => {
         // Send the populated projects in the response
         response.status(200).send(projects); 
 
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//Route for get one project
+router.get('/:project_id', async (request, response) => {
+    try{
+        const { project_id } = request.params; 
+
+        const project = await Project.findById(project_id);
+
+        return response.status(200).send(project); 
     }catch(error){
         console.log(error.message);
         response.status(500).send({ message: error.message });
