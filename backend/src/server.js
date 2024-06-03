850cfda8baf64ada49bbdfba673dc62c3be6ad04
Chii Yuen <chii.yuen@hotmail.com>
2024-06-03 13:53:04 +0800

    Add express app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,13 @@
+import express from "express";
+import cors from "cors";
 import { client } from "./database.js";
+
+const port = 8000;
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
