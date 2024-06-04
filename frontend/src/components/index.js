9b90bf2a7ff3ab944112b8591c640e5e0dc955b1
eesuhn <eason.yihong@gmail.com>
2024-06-04 23:46:12 +0800

    Solved kw, cy

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,9 +1,13 @@
 import Navbar from './Navbar';
 import TaskCard from './TaskCard';
 import AddCard from './AddCard';
+import Work from './work';
+import Lane from './lane';
 
 export {
   Navbar,
   TaskCard,
-  AddCard
+  AddCard,
+  Work,
+  Lane
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,12 @@
 import Test from './Test';
 import Login from './Login';
 import Register from './Register'
-
+import Dashboard from './Dashboard';
 export {
   Test,
   Login,
+  Dashboard,
   Register
 }
+
+
