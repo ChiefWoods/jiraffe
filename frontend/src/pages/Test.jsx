58f31c7be3b160a6cb558cebf60df92f9d745c0a
eesuhn <eason.yihong@gmail.com>
2024-06-04 23:53:23 +0800

    Solved another conflict

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
 import {
   Navbar,
   TaskCard,
   AddCard,
 } from '../components';
-=======
-import { Navbar, TaskCard,Lane,Work } from '../components';
->>>>>>> kw
 
 const Test = () => {
   return (
-    <div>
-<<<<<<< HEAD
-      {/* <AddCard /> */}
-      <Navbar />
-      {/* <TaskCard /> */}
-=======
-      {/* <Navbar /> */}
-      <Work />
->>>>>>> kw
-    </div>
+    <Navbar />
   )
 }
 
