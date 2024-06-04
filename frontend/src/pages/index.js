b7347eb5ea0e1154f749e843610a73a1af03b31b
eesuhn <eason.yihong@gmail.com>
2024-06-04 23:50:44 +0800

    Solved another conflict

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,22 +1,13 @@
 import Test from './Test';
 import Login from './Login';
 import Register from './Register'
-<<<<<<< HEAD
 import Dashboard from './Dashboard';
-export {
-  Test,
-  Login,
-  Dashboard,
-  Register
-=======
 import Settings from './Settings';
 
 export {
   Test,
   Login,
   Register,
+  Dashboard,
   Settings
->>>>>>> eas
 }
-
-
